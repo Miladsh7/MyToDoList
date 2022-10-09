5fdb63803ddac4268f7285d0b7c384b08e6db033
Miladsh7 <80962004+Miladsh7@users.noreply.github.com>
2022-10-10 00:14:08 +0330

    Initial commit

---
+++ app/src/main/java/com/miladsh7/mytodolist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miladsh7.mytodolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
